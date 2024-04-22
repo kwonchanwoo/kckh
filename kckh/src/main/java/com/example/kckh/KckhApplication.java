11611388b1d6c647731c605af74431f0d498d9b2
user <giraffeworld@naver.com>
2024-04-22 20:47:26 +0900

    commit init

---
+++ kckh/src/main/java/com/example/kckh/KckhApplication.java
@@ -0,0 +1,13 @@
+package com.example.kckh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KckhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KckhApplication.class, args);
+	}
+
+}
